8703b30769d821c3bf8850b14daa97d8ffcbb586
Hasan Rafi <mr.k779@outlook.com>
2014-09-03 23:28:37 +0600

    Created Front Shape

--- js/services/routes.js
+++ js/services/routes.js
@@ -1,8 +1,13 @@
 app.config(function($stateProvider, $urlRouterProvider) {
-    $urlRouterProvider.otherwise("/");
+    $urlRouterProvider.otherwise("home");
     $stateProvider
         .state('nav', {
             url: '/',
-            templateUrl: 'templates/nav.html'
+            templateUrl: 'templates/nav.html',
+            controller: 'NavbarCtrl'
+        })
+        .state('nav.home', {
+            url: 'home',
+            templateUrl: 'templates/home.html'
         });
 });
